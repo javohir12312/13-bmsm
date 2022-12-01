9ef9e96f46acaab088f36a1cd923d326c6828598
javohir12312 <abduxalilovjavohir393@gmail.com>
2022-12-01 18:04:22 +0300

    add Error Page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./Error.module.scss"
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className={styles.error__img}>
+      <div className={styles.error__text}>
+        <div className={styles.error__box}>
+          <div class={styles.liquid}>
+            <h2 data-text="404">404</h2>
+          </div>
+          <p data-text="Page not found" className={styles.error__massage}>
+            Page not found
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Error;
